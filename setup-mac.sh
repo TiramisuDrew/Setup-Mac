f8fbf25eadffd119ccbc38c8e66b5d9335a2ba6d
TiramisuDrew <48694394+TiramisuDrew@users.noreply.github.com>
2023-04-09 11:27:54 +0200

    Fix add brew to PATH

--- setup-mac.sh
+++ setup-mac.sh
@@ -10,8 +10,8 @@ echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Ensure Homebrew is in the PATH
-echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
-source ~/.zshrc
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Update Homebrew
 brew update
