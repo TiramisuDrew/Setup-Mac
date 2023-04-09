6e1f2d3371c5546067cb92915b23687a351428c9
TiramisuDrew <48694394+TiramisuDrew@users.noreply.github.com>
2023-04-09 21:35:21 +0200

    Add install for nvm

--- setup-mac.sh
+++ setup-mac.sh
@@ -20,6 +20,11 @@ brew install git
 brew install nvm
 brew install zsh
 brew install mas
+brew install nvm
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # Install .NET Core SDK
 echo "Installing .NET Core SDK"
