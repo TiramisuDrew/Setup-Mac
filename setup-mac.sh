03a337d335557a5ee09d295ed01c7d5f849ca3ef
TiramisuDrew <48694394+TiramisuDrew@users.noreply.github.com>
2023-04-17 16:15:26 +0200

    Refactor setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-# Update and upgrade OS
+# Update and upgrade macOS
 echo "Updating and upgrading macOS..."
 sudo softwareupdate -i -a
 xcode-select --install
+
+# Install oh-my-zsh
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Configure aliases
+read -p "Enter the default username for Homebrew: " default_username
+sed "s/USERNAME_PLACEHOLDER/$default_username/" aliases.zsh > aliases_temp.zsh
+mv aliases_temp.zsh ~/.oh-my-zsh/custom/aliases.zsh
+source ~/.oh-my-zsh/custom/aliases.zsh
 
 # Install Homebrew
 echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-
+# Configure Homebrew environment
+echo "Configuring Homebrew environment..."
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update
 
 # Install essential development tools
@@ -23,44 +34,29 @@ brew install mas
 brew install angular-cli
 brew install nvm
 
+# Configure nvm environment
+echo "Configuring nvm environment..."
 export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
 
-# Install .NET Core SDK
-echo "Installing .NET Core SDK"
+# Install development applications
+echo "Installing development applications..."
 brew install --cask dotnet-sdk
-
-# Install Visual Studio Code
-echo "Installing Visual Studio Code"
 brew install --cask visual-studio-code
-
-# Install Obsidian
-echo "Installing Obsidian"
-brew install --cask obsidian
-
-# Install Iterm2
-echo "Installing Iterm2"
 brew install --cask iterm2
 brew install --cask github
-
-# Install Insomnia
-echo "Installing Insomnia"
 brew install --cask insomnia
 
-# Install oh-my-zsh
-echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cat aliases.zsh >> ~/.oh-my-zsh/custom/aliases.zsh
+# Install productivity applications
+echo "Installing productivity applications..."
+brew install --cask obsidian
 
-# Install Xcode
-echo "Installing Xcode..."
-mas install 497799835
+# Install development tools from Mac App Store
+echo "Installing development tools from Mac App Store..."
+mas install 497799835  # Xcode
 open "/Applications/Xcode.app"
-
-# Install Apple Developer
-echo "Installing Apple Developer"
-mas install 640199958
+mas install 640199958  # Apple Developer
 open "/Applications/Developer.app"
 
 echo "Finished installing all tools and applications!"
