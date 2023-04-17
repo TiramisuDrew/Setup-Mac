3a01bcf7c176d951035089acf288d0d0af946ece
TiramisuDrew <48694394+TiramisuDrew@users.noreply.github.com>
2023-04-17 16:15:11 +0200

    Update aliases.zsh with more aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,36 @@
-# cd
+# Directory navigation
 alias ..='cd ..'
 alias ~='cd ~'
 
-# ls
-alias l='ls'
-alias ll='ls -l'
-alias la='ls -A'
+# ls with color output, human-readable file sizes, and file type indicators
+alias l='ls -GF'
+alias ll='ls -lGhF'
+alias la='ls -AGhF'
 
-# homebrew
-alias brew='sudo -Hu andrew brew'
+# Homebrew with specified user
+alias brew='sudo -Hu USERNAME_PLACEHOLDER brew'
 
-# git
+# Git shortcuts
 alias g='git'
+alias gs='git status'
+alias gd='git diff'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gl='git log --oneline --graph --decorate --all'
+alias gco='git checkout'
 
-# other
+# Grep with color highlighting
+alias grep='grep --color=auto'
+
+# Cat replacement with syntax highlighting (requires bat to be installed)
+alias cat='bat'
+
+# Clear terminal screen
 alias c='clear'
+
+# Homebrew package update and upgrade
+alias brewup='brew update && brew upgrade'
+
+# Open Finder in the current directory
+alias finder='open .'
